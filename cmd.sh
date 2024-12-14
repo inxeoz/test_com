4f684e92f045b37e7cbb68f4cc93dd2a66d54923
inxeoz <pk@pk.com>
2024-12-14 18:10:31 +0530

    configured cmd.sh for windows exe

--- cmd.sh
+++ cmd.sh
@@ -5,15 +5,32 @@ cd build || exit
 cmake ..
 make
 
-###### start of dependent process (program depends on input value (code.ved)
+### getting input and saving ir to llvm file
 ./test_compiler code.ved > output.ll
+
+###### start of dependent process for native (linux i guess)
 ls
 cat output.ll
 llc -filetype=obj output.ll -o output.o
+ls
 clang output.o -o test_program -no-pie
 ls
 ./test_program
-
 #### end of dependent process
+
+
+####for windows
+#sudo apt install clang llvm mingw-w64 ##to install mingw to work with windows system
+
+
+###### start of dependent process for windows
+ls
+cat output.ll
+llc -filetype=obj -mtriple=x86_64-w64-mingw32 output.ll -o output.o
+ls
+x86_64-w64-mingw32-g++ -o test_program.exe output.o -static
+ls
+#### end of dependent process
+
 
 
